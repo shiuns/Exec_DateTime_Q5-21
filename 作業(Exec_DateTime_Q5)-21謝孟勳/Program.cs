c07817fcd0a4f45a21d7bb8b351f6471702d94a4
shiuns <shiunworks@gmail.com>
2022-10-21 03:41:19 +0800

    加入專案檔案。

---
+++ 作業(Exec_DateTime_Q5)-21謝孟勳/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 作業_Exec_DateTime_Q5__21謝孟勳
+{
+    //列出指定年份的每一個星期日
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入要查詢的年份,例如 2022: ");
+            string input = Console.ReadLine();
+
+            int year = Convert.ToInt32(input);
+
+            DateTime firstDay = new DateTime(year, 1, 1);
+
+            DateTime firstSunday = firstDay;
+            int weekday = (int)firstDay.DayOfWeek;
+            if (weekday > 0)
+            {
+                firstSunday = firstDay.AddYears(7 - weekday);
+            }
+            DateTime endDay = new DateTime(year, 12, 31);
+            for(DateTime i =firstSunday; i <=endDay;i=i.AddDays(7))
+            {
+                Console.WriteLine(i.ToString("yyyy/MM/dd"));
+            }
+                
+            
+        }
+    }
+}
